1e49c585b2ed33601b705703afb4d7471f26bf6c
k.chaykin <chaykin.k@gmail.com>
2021-03-27 21:07:04 +0500

    Add vector funcs

--- math/vector.go
+++ math/vector.go
@@ -4,3 +4,32 @@ type Vector struct {
 	X float64
 	Y float64
 }
+
+func (r *Vector) Add(x, y float64) {
+	r.X += x
+	r.Y += y
+}
+
+func (r *Vector) AddVector(v *Vector) {
+	r.X += v.X
+	r.Y += v.Y
+}
+
+func (r *Vector) Sub(x, y float64) {
+	r.X -= x
+	r.Y -= y
+}
+
+func (r *Vector) SubVector(v *Vector) {
+	r.X -= v.X
+	r.Y -= v.Y
+}
+
+func (r *Vector) Set(x, y float64) {
+	r.X = x
+	r.Y = y
+}
+
+func (r *Vector) Clear() {
+	r.Set(0, 0)
+}
---
+++ math/vector_test.go
@@ -0,0 +1,55 @@
+package math
+
+import "testing"
+
+func TestAdd(t *testing.T) {
+	v1 := Vector{X: 10, Y: 20}
+	v1.Add(5, 6)
+
+	if !(v1.X == 15 && v1.Y == 26) {
+		t.Errorf("Invalid add operation\n")
+		return
+	}
+
+	v2 := Vector{X: 100, Y: 200}
+	v1.AddVector(&v2)
+
+	if !(v1.X == 115 && v1.Y == 226 && v2.X == 100 && v2.Y == 200) {
+		t.Errorf("Invalid add vector operation\n")
+	}
+}
+
+func TestSub(t *testing.T) {
+	v1 := Vector{X: 10, Y: 20}
+	v1.Sub(4, 6)
+
+	if !(v1.X == 6 && v1.Y == 14) {
+		t.Errorf("Invalid sub operation\n")
+		return
+	}
+
+	v2 := Vector{X: 100, Y: 200}
+	v1.SubVector(&v2)
+
+	if !(v1.X == -94 && v1.Y == -186 && v2.X == 100 && v2.Y == 200) {
+		t.Errorf("Invalid sub vector operation\n")
+	}
+}
+
+func TestSet(t *testing.T) {
+	v1 := Vector{X: 10, Y: 20}
+	v1.Set(4, 6)
+
+	if !(v1.X == 4 && v1.Y == 6) {
+		t.Errorf("Invalid set operation\n")
+	}
+}
+
+func TestClear(t *testing.T) {
+	v1 := Vector{X: 10, Y: 20}
+	v1.Clear()
+
+	if !(v1.X == 0 && v1.Y == 0) {
+		t.Errorf("Invalid clear operation\n")
+	}
+}
